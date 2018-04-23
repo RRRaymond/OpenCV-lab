82f7314279fe396848f2a98326488f0ba928e0b1
raymondchen <1552702@tongji.edu.cn>
2018-04-23 08:46:00 +0800

    Initial Commit

---
+++ OpenCV-LAB/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV-LAB
+//
+//  Created by RaymondChen on 23/04/2018.
+//  Copyright © 2018 RaymondChen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
